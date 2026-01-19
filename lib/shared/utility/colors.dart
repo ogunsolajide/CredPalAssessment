fc0f5fca7d88b1929fe5a90c746ff9aadf218b7b
oguns babajide <ogunsbabajide@gmail.com>
2026-01-19 18:57:06 +0100

    Project setup

---
+++ lib/shared/utility/colors.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class AppColors {
+  static const secondary = Color(0xFFD0DAFF);
+  static const primary = Color(0xFF274FED);
+  static const inputFieldBackgroundColor= Color(0xFFF1F3FE);
+
+  static const white = Color(0xFFFFFFFF);
+  static const black = Color(0xFF000000);
+  static const textBlack = Color(0xFF1A1A1A);
+  static const textInactive = Color(0xFFB3B3CC);
+}
